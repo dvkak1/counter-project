fe044bb1102709b136e56a52b5cda319a43a8a05
dvkak1 <dv_kakilala@yahoo.com>
2025-01-08 15:36:24 +0800

    counter color added see prev msg

--- app.js
+++ app.js
@@ -20,6 +20,16 @@ btns.forEach(function (btn) {
      else {
       count = 0;
      }
+
+     if(count > 0) {
+        value.style.color = 'green';
+     } else if (count < 0) {
+        value.style.color = 'red';
+     }
+     
+     if (count === 0) {
+        value.style.color = "#222";
+     }
      value.textContent = count;
   });
 });
